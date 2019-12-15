e49d6fda33b21cf395958a2b00dccd8024e9f937
c531784099 <531784099@qq.com>
2019-12-15 21:52:45 +0800

    ceshi

--- qutos/spiders/Testjianshu.py
+++ qutos/spiders/Testjianshu.py
@@ -4,7 +4,6 @@ import  scrapy
 from scrapy.http.response.html import HtmlResponse
 from scrapy.selector.unified import SelectorList
 from qutos.items import QutosItem
-
 class TestJ(scrapy.Spider):
         # 爬虫的名称，不可更改
         name = 'pages1'
@@ -19,7 +18,6 @@ class TestJ(scrapy.Spider):
                       # 'http://quotes.toscrape.com/page/8/',
                       # 'http://quotes.toscrape.com/page/9/',
                       # 'http://quotes.toscrape.com/page/10/',
-
                       ]  # 从这个网址开始执行爬虫，注意默认是http，修改成https
         # scrapy爬虫中不会主动修改页面中的链接，所以自己增加一个类变量用于将相对地址完整成为绝对地址。
         #baseurl = 'https://formoon.github.io'
